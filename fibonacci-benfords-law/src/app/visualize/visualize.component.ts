1bd86eac84004631f589c95f7150cc11f0909a66
Nate Jenson <nate.m.jenson@gmail.com>
2017-12-11 22:41:04 -0600

    set chart data

--- fibonacci-benfords-law/src/app/visualize/visualize.component.ts
+++ fibonacci-benfords-law/src/app/visualize/visualize.component.ts
@@ -9,19 +9,13 @@ import Chart from "frappe-charts/dist/frappe-charts.min.esm"
 export class VisualizeComponent implements OnInit {
 
   data = {
-    labels: ["12am-3am", "3am-6pm", "6am-9am", "9am-12am",
-        "12pm-3pm", "3pm-6pm", "6pm-9pm", "9am-12am"
-    ],
+    labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
     datasets: [
         {
-            title: "Some Data",
-            values: [25, 40, 30, 35, 8, 52, 17, -4]
+            title: "Fibonacci Leading Digit",
+            values: [9,8,7,6,5,4,3,2,1],
         },
-        {
-            title: "Another Set",
-            values: [25, 50, -10, 15, 18, 32, 27, 14]
-        }
-    ]
+    ],
   }
   constructor() { }
 
