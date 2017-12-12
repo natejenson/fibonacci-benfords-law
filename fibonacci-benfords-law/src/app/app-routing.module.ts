d1b257e1649300dc1518d6c9b17e02dbe7430362
Nate Jenson <nate.m.jenson@gmail.com>
2017-12-11 21:05:41 -0600

    add default route

--- fibonacci-benfords-law/src/app/app-routing.module.ts
+++ fibonacci-benfords-law/src/app/app-routing.module.ts
@@ -1,5 +1,16 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VisualizeComponent } from './visualize/visualize.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: 'visualize', component: VisualizeComponent },
+  { path: '',
+    redirectTo: '/welcome',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   exports: [ RouterModule ]
