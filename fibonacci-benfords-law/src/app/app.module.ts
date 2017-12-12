6149570d4e5e6381f9d9f3023cd1b8dd293b56fd
Nate Jenson <nate.m.jenson@gmail.com>
2017-12-11 21:24:30 -0600

    create feed component

--- fibonacci-benfords-law/src/app/app.module.ts
+++ fibonacci-benfords-law/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { VisualizeComponent } from './visualize/visualize.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { FeedComponent } from './feed/feed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    VisualizeComponent
+    VisualizeComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule,
