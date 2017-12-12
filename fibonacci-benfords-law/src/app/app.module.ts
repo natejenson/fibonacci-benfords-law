fc512e648eef854b79f18596d3ee399535667fca
Nate Jenson <nate.m.jenson@gmail.com>
2017-12-11 20:49:20 -0600

    create visualize component

--- fibonacci-benfords-law/src/app/app.module.ts
+++ fibonacci-benfords-law/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { VisualizeComponent } from './visualize/visualize.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    VisualizeComponent
   ],
   imports: [
     BrowserModule
